5ec7b891c586abe186afd59d608a9d51ef6040f1
thanh pham <thanhpph94@gmail.com>
2023-02-11 23:07:08 +0700

    Handle Custom Dropdown - Part II

--- selenium/webdriver/Topic_09_Custome_Dropdown.java
+++ selenium/webdriver/Topic_09_Custome_Dropdown.java
@@ -29,7 +29,7 @@ public class Topic_09_Custome_Dropdown {
 		driver.manage().window().maximize();
 	}
 
-	@Test
+	//@Test
 	public void TC_01_JQuery() {
 		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
 		
@@ -62,6 +62,55 @@ public class Topic_09_Custome_Dropdown {
 		}
 	}
 
+	//@Test
+	public void TC_02_JQuery() {
+		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
+		
+		selectItemDropdown("span#speed-button", "//div[@role='option']", "Fast");
+		sleepInSecond(3);
+		
+		selectItemDropdown("span#speed-button", "//div[@role='option']", "Slow");
+		sleepInSecond(3);
+	}
+	
+	//@Test
+	public void TC_03_ReactJS() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+		
+		selectItemDropdown("i.dropdown.icon", "//div[@class='visible menu transition']//span[@class='text']", "Stevie Feliciano");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[role='alert']")).getText(), "Stevie Feliciano");
+		
+		selectItemDropdown("i.dropdown.icon", "//div[@class='visible menu transition']//span[@class='text']", "Matt");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[role='alert']")).getText(), "Matt");
+	}
+	
+	//@Test
+	public void TC_04_VueJS() {
+		driver.get("https://mikerodham.github.io/vue-dropdowns/");
+		
+		selectItemDropdown("li.dropdown-toggle", "//ul[@class='dropdown-menu']//a", "Second Option");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "Second Option");
+		
+		selectItemDropdown("li.dropdown-toggle", "//ul[@class='dropdown-menu']//a", "Third Option");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "Third Option");
+	}
+	
+	@Test
+	public void TC_05_Editable() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-search-selection/");
+		
+		EditAndSelectItemDropdown("input.search", "//div[@class='selected item']//span", "Angola");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Angola");
+		
+		EditAndSelectItemDropdown("input.search", "//span[@class='text']", "Argentina");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Argentina");
+	}
 	
 	public void sleepInSecond(long timeInSecond) {
 		// tại sao * 1000 vì hàm nhận đơn vị ms nên mình phải đổi từ s → ms, 1000 ms = 1 s (chuyển về ms)
@@ -71,7 +120,68 @@ public class Topic_09_Custome_Dropdown {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	// Tránh lặp lại code nhiều lần, chỉ cần gọi hàm ra để dùng
+	// Hàm thường đi kèm với tham số
+	public void selectItemDropdown (String parentCss, String allItemCss, String expectedCss) {
+		// Click vào 1 thẻ bất kì làm sao cho nó xổ ra hết câc item của dropdownlist
+			driver.findElement(By.cssSelector(parentCss)).click();
+				
+			// Chờ cho tất cả các item được load ra thành công
+			// Locator phải đại diện cho tất cả các item
+			// Lấy đến thẻ chứa text
+			explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemCss)));
+				
+			// Đưa hết tất cả item trong dropdown vào 1 list
+			List<WebElement> speedDropDownList = driver.findElements(By.xpath(allItemCss));
+			
+			// Tìm item xem đúng cái đang cần hay không (dùng vòng lặp duyệt qua)
+			for (WebElement temItem : speedDropDownList) {
+				String itemText = temItem.getText();
+				System.out.println(itemText);
+					
+				if(itemText.equals(expectedCss))
+				{
+					System.out.println("Click vào item");
+					temItem.click();
+					break;
+				}
+				else
+				{
+					System.out.println("Không click vào item");
+				}
+			}
+	}
 		
+	public void EditAndSelectItemDropdown(String parentCss, String allItemCss, String expectedCss) {
+		// Click vào 1 thẻ bất kì làm sao cho nó xổ ra hết câc item của
+		// dropdownlist
+		driver.findElement(By.cssSelector(parentCss)).clear();
+		driver.findElement(By.cssSelector(parentCss)).sendKeys(expectedCss);
+		sleepInSecond(5);
+		// Chờ cho tất cả các item được load ra thành công
+		// Locator phải đại diện cho tất cả các item
+		// Lấy đến thẻ chứa text
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemCss)));
+
+		// Đưa hết tất cả item trong dropdown vào 1 list
+		List<WebElement> speedDropDownList = driver.findElements(By.xpath(allItemCss));
+
+		// Tìm item xem đúng cái đang cần hay không (dùng vòng lặp duyệt qua)
+		for (WebElement temItem : speedDropDownList) {
+			String itemText = temItem.getText();
+			System.out.println(itemText);
+
+			if (itemText.equals(expectedCss)) {
+				System.out.println("Click vào item");
+				temItem.click();
+				break;
+			} else {
+				System.out.println("Không click vào item");
+			}
+		}
+	}
 
 	@AfterClass
 	public void afterClass() {
